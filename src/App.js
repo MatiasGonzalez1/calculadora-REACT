95f6a8964278ce45536087ce7b82ffe5cf9d92f9
Matias Gonzalez <mng_epson@hotmail.com>
2023-02-09 21:09:43 -0300

    feat: add logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 // import logo from './logo.svg';
 import './App.css';
+import Logo from './assets/img/unnamed.png'
 
 function App() {
   return (
     <div className="App">
+      <div className='freecodecamp-logo-contenedor'>
+        <img src={Logo}
+        className='freecodecamp-logo'
+        alt='Logo de freecodecamp' />
+      </div>
     </div>
   );
 }
